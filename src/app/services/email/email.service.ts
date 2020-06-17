0aec61c0ca469c46f76230cfa2fc2a3456bdb29d
Amit Patel <amitkumar7patel@gmail.com>
2020-06-17 15:50:06 -0400

    customDateComparator pseudo-code

--- src/app/services/email/email.service.ts
+++ src/app/services/email/email.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+
+
 import { Email } from '../../models/email';
 import { EMAILS } from './mocks/mock-emails-short';
 import { Name } from '../../models/name';
@@ -27,7 +29,7 @@ export class EmailService {
       const lastA = splitA[splitA.length - 1];
       const lastB = splitB[splitB.length - 1];
 
-      if (lastA < lastB) { 
+      if (lastA < lastB) {
         return -1;
       }
       if (lastA > lastB) {
@@ -48,6 +50,18 @@ export class EmailService {
     }
 
     customEmailDateComparator(itemA: string, itemB: string): number {
+
+        // Date format is date: '{{moment(this.date(new Date(2014, 0, 1), new Date())).format("LLLL")}}'
+        /*
+          import moment from 'moment'
+          import _ from 'lodash'
+          const dateArray = [{date:"2018-05-11"},{date:"2018-05-12"},{date:"2018-05-10"}]
+          const sortedArray = _.orderBy(array, (o: any) => {
+            return moment(o.date.format('YYYYMMDD');
+          }, ['asc']);
+          console.log(sortedDates)
+        */
+
         return itemA > itemB ? 1 : -1;
     }
 }
